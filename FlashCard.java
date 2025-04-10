282d2448744184de45ca46eb161a30ccf5d3c012
smrive4 <193434163+smrive4@users.noreply.github.com>
2025-04-10 15:05:35 -0500

    Add FlashCard Class and FlashCardSet

---
+++ FlashCard.java
@@ -0,0 +1,51 @@
+
+/**
+ * FlashCard Class
+ */
+public class FlashCard{
+    private String term; /** Term*/
+    private String definition; /** Definition*/
+
+    /**
+     * Constuctor
+     * 
+     * @param term term for the flashcard
+     * @param definition definition of the term
+     */
+    public FlashCard(String term, String definition) {
+        this.term = term;
+        this.definition = definition;
+    }
+
+    /**
+     * Getter to get the term
+     */
+    public String getTerm(){
+        return definition;
+    }
+    
+    /**
+     * Getter to get the definition
+     */
+    public String getDef(){
+        return this.term;
+    }
+
+    /**
+     * Setter to set the term of the flashcard
+     * 
+     * @param term term of the flashcard
+     */
+    public void updateTerm(String term){
+        this.term = term;
+    }
+
+    /**
+     * Setter to set the definition
+     * 
+     * @param definition definition of the term
+     */
+    public void updateDef(String definition){
+        this.definition = definition;
+    }
+}
